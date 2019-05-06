670b70f67d42ae9dd3a38c830db76bf6b5939cb7
DannySuarez <suarezd10@gmail.com>
2019-05-06 14:29:56 -0700

    setup the map page

---
+++ src/map/map.js
@@ -0,0 +1,21 @@
+import api from '../services/api.js';
+
+//refernce needed Dom elements
+const character = document.getElemtnById('character');
+const powerUps = document.getElementById('power-ups');
+const lives = document.getElementById('lives');
+const coins = document.getElementById('coins');
+
+//initialize profile with user
+const user = api.getUser();
+
+//no user? go back to sign up
+if(!user) {
+    window.location = './';
+}
+
+//populate the profile
+character.textContent = user.character;
+powerUps.textContent = user.powerUps;
+lives.textContent = user.lives;
+coins.textContent = user.coins;
