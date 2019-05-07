db1884457c18edefe4872d820cf0695205fff0be
Erin Gurley <erinmgurley@gmail.com>
2019-05-07 13:50:10 -0700

    added quest-data.js page

---
+++ src/services/quest-data.js
@@ -0,0 +1,14 @@
+const cloudWorld = {
+    //quest 1 data
+};
+const tunnelWorld = {
+    //quest 2 data
+};
+
+const bowsersWorld = {
+    //quest 3 data
+};
+
+const quests = [cloudWorld, tunnelWorld, bowsersWorld];
+
+export default quests;
