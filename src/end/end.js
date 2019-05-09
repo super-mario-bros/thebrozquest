b5d4ef6cb5193b7ac1d5c2f34e3424c25780cfc1
DannySuarez <suarezd10@gmail.com>
2019-05-09 16:28:29 -0700

    load ended game page, completed user testing

--- src/end/end.js
+++ src/end/end.js
@@ -1,8 +1,26 @@
 import loadProfile from '../load-profile.js';
+import api from '../services/api.js';
+import scoreLives from '../end/score-lives.js';
+import scoreCoins from '../end/score-coins.js';
+import resultMessages from './result-messages.js';
 
 loadProfile();
 
 const user = api.getUser();
-const storyDisplay = document.getElementById('win-lose-message');
 const winLose = document.getElementById('win-lose');
 
+const coinResult = scoreCoins(user.coins);
+const livesResult = scoreLives(user.lives);
+
+let messages;
+if(livesResult === 'alive' && coinResult === 'win') {
+    messages = resultMessages['alive'];
+}
+else if(livesResult === 'alive') {
+    messages = resultMessages['broke'];
+}
+else { 
+    messages = resultMessages['dead'];
+}
+
+winLose.textContent = messages;
---
+++ src/end/result-messages.js
@@ -0,0 +1,7 @@
+const resultMessages = {
+    dead: 'You didn\'t survive or save Toad.',
+    alive: 'Congrats! You are a Hero!!! You saved Toad!',
+    broke: 'Sorry, you don\'t have enough coins to save Toad.'
+};
+
+export default resultMessages;
